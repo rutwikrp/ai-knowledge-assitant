d54ed264cf378ae7dfd062bf057451cddf631d61
rutwikrp <rutwikpatil73@gmail.com>
2026-06-22 23:02:26 +0530

    new embedding

---
+++ backend/embedding/embedding_service.py
@@ -0,0 +1,8 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer(
+    "BAAI/bge-small-en-v1.5"
+)
+
+def get_embedding(text):
+    return model.encode(text).tolist()
